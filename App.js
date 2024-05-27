6f43b24da22527a2b7a720f4fb098595e5e4ae1a
jimmy southavy <you@example.com>
2024-05-27 11:24:47 +0700

    Commit

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.counterContainer}>
+        <Text >Southavy sengdavong</Text>
+        <Text style={styles.counterText}>Counter {count}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => setCount(count + 1)}>
+            <Text style={styles.buttonText}>Increment</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setCount(count - 1)}>
+            <Text style={styles.buttonText}>Decrement</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => setCount(0)}>
+            <Text style={styles.buttonText}>Reset</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    
+  },
+  counterContainer: {
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  counterText: {
+    fontSize: 62,
+    marginBottom: 20,
+    color: '#333',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 15,
+    borderRadius: 5,
+    marginBottom: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
 });
+
+export default App;
